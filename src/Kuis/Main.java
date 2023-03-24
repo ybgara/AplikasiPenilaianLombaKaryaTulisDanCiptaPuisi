f3d53f85497b97c817aa99b04fdffac2a08c4dd8
Tegar Wibisana <95567621+ybgara@users.noreply.github.com>
2023-03-25 01:15:52 +0700

    Add files via upload

---
+++ src/Kuis/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kuis;
+
+/**
+ *
+ * @author G a r a
+ */
+public class Main {
+    public static void main(String[] args){
+    
+        new KategoriLomba().setVisible(true);
+        
+    };
+}
